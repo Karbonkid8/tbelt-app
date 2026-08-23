62f0c8dddc38a5aff72c01b04231e4ef953d8d5e
Jason Beckmann <jasonbeckmann@me.com>
2026-08-23 04:56:12 -0600

    Add custom T-Belt PWA icons

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,20 +1,26 @@
-]import { ScrollViewStyleReset } from 'expo-router/html';
+import { ScrollViewStyleReset } from 'expo-router/html';
 import type { PropsWithChildren } from 'react';
 
-export default function Root({
-  children,
-}: PropsWithChildren) {
+export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
 
         <meta
+          httpEquiv="X-UA-Compatible"
+          content="IE=edge"
+        />
+
+        <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        <meta name="theme-color" content="#111111" />
+        <meta
+          name="theme-color"
+          content="#111111"
+        />
 
         <link
           rel="manifest"
